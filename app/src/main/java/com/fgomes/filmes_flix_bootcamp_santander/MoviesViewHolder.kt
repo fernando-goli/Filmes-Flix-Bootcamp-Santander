85eb7ee1a8ed340609e357b502971c041fea989d
fernando gomes <fernando.gmsoli@gmail.com>
2021-08-27 17:04:55 -0300

    Created adapter and recyclerview

---
+++ app/src/main/java/com/fgomes/filmes_flix_bootcamp_santander/MoviesViewHolder.kt
@@ -0,0 +1,6 @@
+package com.fgomes.filmes_flix_bootcamp_santander
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class MoviesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
